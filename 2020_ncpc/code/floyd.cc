b4cec83b85dd403550b6d9ee1686bb8def304f6a
bill <phongbinh2511@gmail.com>
2020-10-04 14:45:48 +0800

    [master] Removed all non-UTF8 characters.

--- 2020_ncpc/code/floyd.cc
+++ 2020_ncpc/code/floyd.cc
@@ -18,7 +18,6 @@ int main()
             cin >> before[i][j] ;
     }
 
-// 上面程式註解
     for(int i = 0 ; i < n ; i++){
         for(int j = i+1 ; j < n ; j++){
             int sum = 0 ;
--- 2020_ncpc/code/max_flow.cc
+++ 2020_ncpc/code/max_flow.cc
@@ -14,7 +14,7 @@ void augment(int v, int minEdge)
 
 // inside int main(): set up 'res', 's', and 't' with appropriate values
     mf = 0;
-    while (true)    // O(V^3 * E) Edmonds Karp’s algorithm
+    while (true)    // O(V^3 * E) Edmonds Karp's algorithm
     {
         f = 0;
         vi dist(MAX_V, INF); dist[s] = 0;
@@ -28,8 +28,8 @@ void augment(int v, int minEdge)
                 if (res[u][v] > 0 && dist[v] == INF)
                     dist[v] = dist[u] + 1, q.push(v), p[v] = u;
         }
-        augment(t, INF);   // find the min edge weight ‘f’ in this path, if any
-        if (f == 0) break; // we cannot send any more flow (‘f’ = 0), terminate
+        augment(t, INF);   // find the min edge weight 'f' in this path, if any
+        if (f == 0) break; // we cannot send any more flow ('f' = 0), terminate
         mf += f;           // we can still send a flow, increase the max flow!
     }
     printf("%d\n", mf);
