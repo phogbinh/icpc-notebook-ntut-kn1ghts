afe7f4a671628249c540cb5ebb005b4a3f390b2f
bill <phongbinh2511@gmail.com>
2020-10-21 16:37:16 +0800

    [master] Did minor modifications on floyd warshall algorithm.

--- 2020_icpc_taiwan_regional/code/apsp.cc
+++ 2020_icpc_taiwan_regional/code/apsp.cc
@@ -1,10 +1,9 @@
 // All-Pairs Shortest Paths (APSP) solved with Floyd Warshall O(V^3).
 // inside int main()
-    // precondition: AdjMat[i][j] contains the weight of edge (i, j)
-    // or INF (1B) if there is no such edge
-    // AdjMat is a 32-bit signed integer array
-    // let p be a 2D parent matrix, where p[i][j] is the last vertex before j
-    // on a shortest path from i to j, i.e. i -> ... -> p[i][j] -> j
+    // Precondition: AdjMat[i][j] contains the weight of edge (i, j) or INF (1B)
+    // if there is no such edge ('AdjMat' is a 32-bit signed integer array).
+    // Let 'p' be 2D parent matrix, where p[i][j] is the last vertex before j on
+    // a shortest path from i to j, i.e. i -> ... -> p[i][j] -> j.
     for (int i = 0; i < V; ++i)
         for (int j = 0; j < V; ++j)
             p[i][j] = i;    // initialize the parent matrix
@@ -20,5 +19,5 @@
 void printPath(int i, int j)
 {
     if (i != j) printPath(i, p[i][j]);
-    printf(" %d", j);
+    printf("%d ", j);
 }
