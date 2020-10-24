b0a8d41951d94509d0a396ca8cea47f1f77ceea3
bill <phongbinh2511@gmail.com>
2020-10-24 18:17:21 +0800

    [master] Updated Edmonds Karp to utilize adjacency list hence reducing time complexity.

--- 2020_icpc_taiwan_regional/code/max_flow.cc
+++ 2020_icpc_taiwan_regional/code/max_flow.cc
@@ -12,9 +12,10 @@ void augment(int v, int minEdge)
     }
 }
 
-// inside int main(): set up 'res', 's', and 't' with appropriate values
+// inside int main(): set up 'AL', 'res', 's', and 't' with appropriate values
+// remember to add backward edges to 'AL'
     mf = 0;
-    while (true)    // O(V^3 * E) Edmonds Karp's algorithm
+    while (true)    // O(V * E^2) Edmonds Karp's algorithm
     {
         f = 0;
         vi dist(MAX_V, INF); dist[s] = 0;
@@ -24,9 +25,12 @@ void augment(int v, int minEdge)
         {
             int u = q.front(); q.pop();
             if (u == t) break; // immediately stop BFS if we already reach sink t
-            for (int v = 0; v < MAX_V; ++v)
+            for (int i = 0; i < (int)AL[u].size(); ++i)
+            {
+                int v = AL[u][i];    // vector< vi > [A]djacency [L]ist
                 if (res[u][v] > 0 && dist[v] == INF)
                     dist[v] = dist[u] + 1, q.push(v), p[v] = u;
+            }
         }
         augment(t, INF);   // find the min edge weight 'f' in this path, if any
         if (f == 0) break; // we cannot send any more flow ('f' = 0), terminate
